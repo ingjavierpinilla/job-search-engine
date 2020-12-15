17ac67fc04e90097f7542a53597b19e3ba8b13fb
Javier <ing.jmpinillag@gmail.com>
2020-12-15 16:32:31 +0100

    model Search created

--- tech_test/tech_test/settings.py
+++ tech_test/tech_test/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #own
+    'search',
+    'you_may_know',
 ]
 
 MIDDLEWARE = [
