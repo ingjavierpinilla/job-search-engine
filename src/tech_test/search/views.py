8b8e198e2611a5c1dde2c6e988b9558e23949e4d
Javier <ing.jmpinillag@gmail.com>
2020-12-15 19:41:31 +0100

    views home and search_bar added

--- src/tech_test/search/views.py
+++ src/tech_test/search/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def home(request):
+    context={'asd':12}
+    return render(request, "search/index.html", context)
+
+def search_bar(request):
+    print(request)
+    if request.method == "POST":
+        current_date = timezone.now()
+        content = request.POST["content"]
+        
+    return HttpResponseRedirect("/")
