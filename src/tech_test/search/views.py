29a32b78cb62d73b72dd5621e6550b6253910a58
Javier <ing.jmpinillag@gmail.com>
2020-12-16 22:22:09 +0100

    Job db deleted in each search and get method used the database is delete to get new results, de dictionary is now consulted using get

--- src/tech_test/search/views.py
+++ src/tech_test/search/views.py
@@ -19,34 +19,47 @@ def get_query(request):
     else:
         remote_search = ''
     jobs = search_job(size = 9, remote = True, skills = ['Software Development'])
-
-    context = {'job_search': job_search, 'place_search': place_search, 'remote_search': remote_search, 'jobs': jobs}
+    Job.objects.all().delete()
+    parse_jobs(jobs)
+    job_items = Job.objects.all()
+    context = {'job_search': job_search, 'place_search': place_search, 'remote_search': remote_search, 'job_items': job_items}
 
     return render(request, "search/search_result.html",context)
 
 def parse_jobs(jobs):
     for job in jobs:
-        id = job['id']
-        objective = job['objective']
-        type = job['type']
-        organization_name = job['organizations'][0]['name']
-        organization_picture = job['organizations'][0]['picture']
-        locations = job['locations']
-        remote = job['remote']
+        id = job.get('id')
+        objective = job.get('objective')
+        type = job.get('type')
+        organization_name = job.get('organizations')[0].get('name')
+        organization_picture = job.get('organizations')[0].get('picture')
+        locations = job.get('locations')
+        remote = job.get('remote')
         if remote:
             locations.append("Remote")
         if len(locations) != 0:
             locations = ', '.join(locations)
         skills =[]
-        for skill in job['skills']:
-            skills.append(skill['name'])
+        for skill in job.get('skills'):
+            skills.append(skill.get('name'))
         skills = ', '.join(skills)
-        min = str(job['compensation']['data']["minAmount"])
-        max = str(job['compensation']['data']["maxAmount"])
-        curr = str(job['compensation']['data']["currency"])
-        per = str(job['compensation']['data']["periodicity"])
-        compensation = f'{min} - {max} {curr}/{per}'
-
-        obj = Todo.objects.create(_id = id, objective = objective, type = type, organization_name = organization_name,
+        if job.get('compensation').get('visible'):
+            min = str(job.get('compensation').get('data').get('minAmount'))
+            max = str(job.get('compensation').get('data').get('maxAmount'))
+            curr = str(job.get('compensation').get('data').get('currency'))
+            per = str(job.get('compensation').get('data').get('periodicity'))
+            if min == 'None':
+                max = max.split('.', 1)[0]
+                compensation = f'{max} {curr}/{per}'
+            elif max == 'None':
+                min = min.split('.', 1)[0]
+                compensation = f'{min} {curr}/{per}'
+            else:
+                min = min.split('.', 1)[0]
+                max = max.split('.', 1)[0]
+                compensation = f'{min} - {max} {curr}/{per}'
+        else:
+            compensation = ''
+        obj = Job.objects.create(_id = id, objective = objective, type = type, organization_name = organization_name,
         organization_picture = organization_picture, locations = locations, remote = remote, skills = skills,
         compensation = compensation)
