890e5f9ffe91e5d17fffa3f3699bff8bce56f4ca
Javier <ing.jmpinillag@gmail.com>
2020-12-16 22:21:35 +0100

    appname: search added

--- src/tech_test/search/urls.py
+++ src/tech_test/search/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import search, get_query
 
-
+appname = 'search'
 urlpatterns = [
     path('', search, name = 'search'),
     path('get_query/', get_query, name ='get_query'),
