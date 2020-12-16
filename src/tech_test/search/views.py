fbba2148a581a5839c01992d50356e94fb2ffbe4
Javier <ing.jmpinillag@gmail.com>
2020-12-16 20:29:07 +0100

    checkbox status added to context

--- src/tech_test/search/views.py
+++ src/tech_test/search/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .API import search_job
 
 # Create your views here.
 def search(request):
-    context ={'job': '','place': ''}
-    return render(request, "search/search_home.html",context)
+    context ={'job_search': '','place_search': '', 'remote_search': ''}
+    return render(request, "search/search_home.html", context)
 
 def get_query(request):
 
-    context = {'job':request.GET['job']}
+    job_search = request.GET.get('job_search',"")
+    place_search = request.GET.get('place_search',"")
+    if request.GET.get('remote_search',"") == 'on':
+        remote_search = 'checked'
+    else:
+        remote_search = ''
+    jobs = search_job(size = 6, remote = False, skills = ['Marketing', 'Communication'])
+    context = {'job_search': job_search, 'place_search': place_search, 'remote_search': remote_search, 'jobs': jobs}
 
     return render(request, "search/search_result.html",context)
