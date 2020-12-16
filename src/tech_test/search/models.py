63c0f0b0dbe07614c875794dcddc3f28d23e734a
Javier <ing.jmpinillag@gmail.com>
2020-12-16 19:22:24 +0100

    model Search deleted

--- src/tech_test/search/models.py
+++ src/tech_test/search/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Search(models.Model):
-    """[summary]
-        Search model
-        [attributes]:
-            main_search: String
-            attributes: Dictionary
-    """
-    main_search = ""
-    attributes ={}
-    def __str__(self):
-        return self.main_search
