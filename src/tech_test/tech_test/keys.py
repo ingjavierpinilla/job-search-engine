6a014c3db60d1813d1b0dd560d9ba04416ef7ad4
Javier <ing.jmpinillag@gmail.com>
2020-12-17 18:07:53 +0100

    Heroku changes

--- src/tech_test/tech_test/keys.py
+++
@@ -1,1 +0,0 @@
-secret_key ='mp*=g%!2h!p&ys!1&7(rdo!)tm4)&=0su#aoi^9gl61m%393^='
--- src/tech_test/tech_test/settings.py
+++ src/tech_test/tech_test/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from .keys import secret_key
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #own
     'search',
-    'you_may_know',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
